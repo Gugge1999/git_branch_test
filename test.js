5550d9c3f05b517069af4886d5242e68666d4637
Gugge1999 <davidgust99@gmail.com>
2020-09-28 17:01:59 +0200

    Added function

---
+++ test.js
@@ -0,0 +1,13 @@
+function msToTime(reloadTime) {
+  var seconds = Math.floor((reloadTime / 1000) % 60);
+  var minutes = Math.floor((reloadTime / (1000 * 60)) % 60);
+  var hours = Math.floor((reloadTime / (1000 * 60 * 60)) % 24);
+
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  return console.log(`${hours}:${minutes}:${seconds}`);
+}
+
+msToTime(10000);

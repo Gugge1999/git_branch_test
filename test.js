0ec5a64b199f44ef89ec21a57f609177bbf8b12f
Gugge1999 <davidgust99@gmail.com>
2020-09-28 18:21:20 +0200

    final feature1 test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 function msToTime(reloadTime) {
   var seconds = Math.floor((reloadTime / 1000) % 60);
   var minutes = Math.floor((reloadTime / (1000 * 60)) % 60);
-  var hours = Math.floor((reloadTime / (1000 * 60 * 60)) % 24);
+  var asdf = Math.floor((reloadTime / (1000 * 60 * 60)) % 24);
 }
 
 msToTime(10000);

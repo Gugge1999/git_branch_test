5ddd98676ed57b2c8432e0643806790dc10bff61
Gugge1999 <davidgust99@gmail.com>
2020-09-28 19:16:02 +0200

    experimal feature 2

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 function msToTime(reloadTime) {
   var seconds = Math.floor((reloadTime / 1000) % 60);
   var minutes = Math.floor((reloadTime / (5000 * 60)) % 60);
-  var experimental = Math.floor((reloadTime / (1000 * 60 * 60)) % 24);
-  var newVariable;
+  var experimental2 = Math.floor((reloadTime / (1000 * 60 * 60)) % 24);
+  var newVariable2;
 }
 
 msToTime(10000);
